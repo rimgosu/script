d5ee24026e6ef458a097d164ebf1d1a920b176a3
ji-hyup <newnyup@gmail.com>
2025-11-05 16:21:14 +0900

    install zsh

---
+++ install-zsh.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y zsh curl git
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+awk '
+/^plugins=\(/ {
+    print "plugins=(git zsh-autosuggestions zsh-syntax-highlighting)"
+    if (!/\)/) {
+        while (getline > 0 && !/\)/) {}
+    }
+    next
+}
+{print}
+' ~/.zshrc > ~/.zshrc.tmp && mv ~/.zshrc.tmp ~/.zshrc
+
+if [ -n "$SUDO_USER" ]; then
+    chown $SUDO_USER ~/.zshrc
+fi
+
+chsh -s $(which zsh)
