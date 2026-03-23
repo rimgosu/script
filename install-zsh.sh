ffd23f04027b6ac62926dc1a92de01c19e6a2a53
ji-hyup <newnyup@gmail.com>
2026-03-23 10:58:45 +0900

    username@servername

--- install-zsh.sh
+++ install-zsh.sh
@@ -24,8 +24,8 @@ fi
 
 sudo locale-gen ko_KR.UTF-8
 grep -q 'LC_ALL=ko_KR.UTF-8' ~/.zshrc || echo 'export LC_ALL=ko_KR.UTF-8' >> ~/.zshrc
-grep -q "PROMPT='%m@ '" ~/.zshrc || cat >> ~/.zshrc << 'EOF'
-PROMPT='%m@ '$PROMPT
+grep -q "PROMPT='%n@%m '" ~/.zshrc || cat >> ~/.zshrc << 'EOF'
+PROMPT='%n@%m '$PROMPT
 EOF
 
 zsh -c "source ~/.zshrc"
