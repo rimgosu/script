ba9c3b6c922269a2ca411168034723a3ee865783
ji-hyup <newnyup@gmail.com>
2026-05-29 14:03:53 +0900

    mac f18, application도 매핑되도록 수정

--- mac-f18.sh
+++ mac-f18.sh
@@ -1,7 +1,16 @@
 mkdir -p /Users/Shared/bin
-echo '''#!/bin/sh\nhidutil property --set '\'{\"UserKeyMapping\":\[\{\"HIDKeyboardModifierMappingSrc\":0x7000000e7,\"HIDKeyboardModifierMappingDst\":0x70000006d\}\]\}\''''' > /Users/Shared/bin/userkeymapping
+
+# 키 리매핑 스크립트 생성
+#  - 오른쪽 Cmd(0x7000000e7) -> F18(0x70000006d)
+#  - Application 키(0x700000065) -> fn/Globe(0xff00000003)
+cat <<'EOF' > /Users/Shared/bin/userkeymapping
+#!/bin/sh
+hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x7000000e7,"HIDKeyboardModifierMappingDst":0x70000006d},{"HIDKeyboardModifierMappingSrc":0x700000065,"HIDKeyboardModifierMappingDst":0xff00000003}]}'
+EOF
 chmod 755 /Users/Shared/bin/userkeymapping
-sudo cat<<: >/Users/Shared/bin/userkeymapping.plist
+
+# 부팅 시 자동 적용되도록 LaunchAgent 등록
+sudo cat <<: >/Users/Shared/bin/userkeymapping.plist
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
--- readme.md
+++ readme.md
@@ -59,7 +59,7 @@ curl -fsSL https://tailscale.com/install.sh | sh
 ## mac 환경 설정
 
 - 한영키 매핑: <https://soobysu.tistory.com/175>
-- right cmd -> f18
+- right cmd -> f18, application -> fn (globe)
 
 ```sh
 curl https://raw.githubusercontent.com/rimgosu/script/refs/heads/main/mac-f18.sh | bash
